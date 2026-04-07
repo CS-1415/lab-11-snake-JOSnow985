d87458fff2b58f7fb4ae31402792fa408ba5d676
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-06 21:02:15 -0600

    Corrected name of Cell class file, oops

--- Lab11/obj/Debug/net8.0/Lab11.AssemblyInfo.cs
+++ Lab11/obj/Debug/net8.0/Lab11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6985a26b8287d9ca2d9a8e132a6755037527af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87798e3c24ac53e6ff8478a32f8be6f4ac927382")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
