8aa918027a8e7ceefcbfb6231dad524e42eb8ec4
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-07 22:56:38 -0600

    Snake Test implementations

--- Lab11.Tests/SnakeTests.cs
+++ Lab11.Tests/SnakeTests.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework.Internal;
+
 namespace Lab11.Tests;
 
 public class SnakeTests
@@ -10,38 +12,64 @@ public class SnakeTests
     {
         Board testBoard =  new();
         // A snake instance that starts at the top left corner and is facing east
-        Snake testSnake = new("testPlayer", [new(0,0), new(0,1), new(0,2)], 'E', ref testBoard);
+        Snake testSnake = new("testPlayer", [new(0,0), new(1,0), new(2,0)], 'E', ref testBoard);
     }
 
     // --- Tests for turning each possible direction ---
     [Test]
     public void TurnNorthTest()
     {
-        Assert.Pass();
+        testSnake.TurnDirection('N');
+        Assert.That(testSnake.CurrentDirection, Is.EqualTo('N'));
     }
 
     [Test]
     public void TurnEastTest()
     {
-        Assert.Pass();
+        testSnake.TurnDirection('E');
+        Assert.That(testSnake.CurrentDirection, Is.EqualTo('E'));
     }
 
     [Test]
     public void TurnSouthTest()
     {
-        Assert.Pass();
+        testSnake.TurnDirection('S');
+        Assert.That(testSnake.CurrentDirection, Is.EqualTo('S'));
     }
 
     [Test]
     public void TurnWestTest()
     {
-        Assert.Pass();
+        testSnake.TurnDirection('W');
+        Assert.That(testSnake.CurrentDirection, Is.EqualTo('W'));
     }
 
     // --- Test for moving a snake forward in the direction it's looking ---
     [Test]
     public void MoveForwardTest()
     {
-        Assert.Pass();
+        // With a freshly constructed Snake instance, moving forward should return true (success)
+        if (testSnake.MoveForward())
+        {
+            bool lastCellRemoved = true;
+            bool newCellAdded = false;
+
+            foreach (Cell cell in testSnake.OccupiedCells)
+            {
+                if (cell.X == 0 && cell.Y == 0)
+                {
+                    lastCellRemoved = false;
+                }
+                else if (cell.X == 3 && cell.Y == 0)
+                {
+                    newCellAdded = true;
+                }
+            }
+            if (lastCellRemoved == true && newCellAdded == true)
+            {
+                Assert.Pass();  // Test passes if the old tail cell has been removed and the new head cell has been added
+            }
+        }
+        Assert.Fail();  // If we got here at all that means the test has failed
     }
 }
