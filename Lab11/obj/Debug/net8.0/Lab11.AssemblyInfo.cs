c6a6e3d9143ea8a9313a2af970ff5124ca402046
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-08 20:19:24 -0600

    Tests passing

--- Lab11/obj/Debug/net8.0/Lab11.AssemblyInfo.cs
+++ Lab11/obj/Debug/net8.0/Lab11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e4446fa112c52b41cf17d579d709fc8701e2c06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c3e8fa9a59f587e8db37835abf67d3577d6490")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
