d9c3e8fa9a59f587e8db37835abf67d3577d6490
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-08 19:30:17 -0600

    basic cell struct test

--- Lab11.Tests/CellTests.cs
+++ Lab11.Tests/CellTests.cs
@@ -2,14 +2,18 @@ namespace Lab11.Tests;
 
 public class CellTests
 {
+    Cell testCell = new(2,1);
+
     [SetUp]
     public void Setup()
     {
     }
 
+    // I don't really know what else to test about the Cell struct
     [Test]
-    public void Test1()
+    public void CellPropertyTest()
     {
-        Assert.Pass();
+        Assert.That(testCell, Has.Property("X").EqualTo(2));
+        Assert.That(testCell, Has.Property("Y").EqualTo(1));
     }
 }
