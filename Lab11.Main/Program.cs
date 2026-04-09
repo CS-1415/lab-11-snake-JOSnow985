baf0e24f8c516cd0a34f47c0d7e75bdb3bc30afd
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-08 22:17:42 -0600

    Move assignment tips to the top

--- Lab11.Main/Program.cs
+++ Lab11.Main/Program.cs
@@ -1,4 +1,21 @@
 ﻿// Jaden Olvera, CS-1410, Lab 11: SNAKES!
+
+/*
+Assignment tips:
+Write the output to the console move the cursor with Console.SetCursorPosition then use Console.Write to print a character at that position. 
+
+I used the following to allow me to check keyboard input and to update characters at arbitrary positions on the screen:
+	System.Console.ReadKey
+	System.ConsoleKey.UpArrow
+	System.ConsoleKey.D
+	System.Console.SetCursorPosition
+	System.Console.Write
+
+The following might help if you choose to have the snakes advance at time intervals rather than only when a button is pressed:
+    System.Console.KeyAvailable 
+    System.Threading.Thread.Sleep
+*/
+
 using Lab11;
 
 Console.Clear();
@@ -26,18 +43,3 @@ Snake blueSnake = new(blueName, [new(gameBoard.Width,gameBoard.Length), new(game
 gameBoard.Snakes.Add(redSnake);
 gameBoard.Snakes.Add(blueSnake);
 
-/*
-Assignment tips:
-Write the output to the console move the cursor with Console.SetCursorPosition then use Console.Write to print a character at that position. 
-
-I used the following to allow me to check keyboard input and to update characters at arbitrary positions on the screen:
-	System.Console.ReadKey
-	System.ConsoleKey.UpArrow
-	System.ConsoleKey.D
-	System.Console.SetCursorPosition
-	System.Console.Write
-
-The following might help if you choose to have the snakes advance at time intervals rather than only when a button is pressed:
-    System.Console.KeyAvailable 
-    System.Threading.Thread.Sleep
-*/
