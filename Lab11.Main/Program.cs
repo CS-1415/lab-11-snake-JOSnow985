85405ad9e1f8b60633a4f19a405f73d5446fe36f
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-08 21:26:45 -0600

    Collect snake names and set up game objects

--- Lab11.Main/Program.cs
+++ Lab11.Main/Program.cs
@@ -1,5 +1,30 @@
 ﻿// Jaden Olvera, CS-1410, Lab 11: SNAKES!
-Console.WriteLine("Hello, this is a multiplayer snake game!\nOne player will control their snake with WASD, the other with the arrow keys!\nAvoid the other snake and the walls while eating the apple to grow.\nThe winner will be printed at the end of the game.");
+using Lab11;
+
+Console.Clear();
+Console.WriteLine("Hello, this is a multiplayer snake game!\nThe blue snake is controlled with WASD, the red snake with the arrow keys!\nAvoid the other snake and the walls while eating the apple to grow.\nThe winner will be printed at the end of the game.\n");
+
+// Collect the snake names for later printing
+Console.WriteLine("What should the red snake be named?");
+string redName = "";
+while (redName == "")
+{
+	redName = Console.ReadLine()!;
+}
+
+Console.WriteLine("What should the blue snake be named?");
+string blueName = "";
+while (blueName == "")
+{
+	redName = Console.ReadLine()!;
+}
+
+// Set up board and snakes
+Board gameBoard = new();
+Snake redSnake = new(redName, [new(0,0), new(1,0), new(2,0)], 'E', ref gameBoard);
+Snake blueSnake = new(blueName, [new(gameBoard.Width,gameBoard.Length), new(gameBoard.Width - 1,gameBoard.Length), new(gameBoard.Width - 2,gameBoard.Length)], 'W', ref gameBoard);
+gameBoard.Snakes.Add(redSnake);
+gameBoard.Snakes.Add(blueSnake);
 
 /*
 Assignment tips:
